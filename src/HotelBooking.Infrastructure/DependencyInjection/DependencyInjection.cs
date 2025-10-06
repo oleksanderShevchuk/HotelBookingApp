3f65cffbb2de55f2ac92918835f5d00320b77b94
OleksandrShevchukDev5 <dev5@kpp-centr.com.ua>
2025-10-06 12:59:26 +0300

    feat(infrastructure): register ApplicationDbContext with MySQL in DI and configure connection in appsettings.json

---
+++ src/HotelBooking.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using HotelBooking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBooking.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        return services;
+    }
+}
